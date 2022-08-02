f59f774ef3a205b3a10be0f86ab916cf8bb63786
Vyacheslav Melovatskiy <slavikne@rambler.ru>
2022-08-03 01:13:35 +0300

    Edit views

--- backend/views.py
+++ backend/views.py
@@ -49,7 +49,7 @@ class RegisterAccount(APIView):
                 return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
             else:
                 # проверяем данные для уникальности имени пользователя
-                request.data._mutable = True
+                # request.data._mutable = True
                 request.data.update({})
                 user_serializer = UserSerializer(data=request.data)
                 if user_serializer.is_valid():
